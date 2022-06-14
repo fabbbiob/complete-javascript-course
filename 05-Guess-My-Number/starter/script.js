2e56f9c0a643bf28bfc069e1b393c2a1e51bff06
Fabio Bongiovanni <Fabio.Bongiovanni@kantar.com>
2022-06-14 15:46:28 +0200

    progress

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,6 @@
 'use strict';
+const $ = (itm) => document.querySelector(itm);
+let item = document.querySelector(".message").textContent;
+console.log(item);
+let item2 = $("#mahh").textContent = "Let's guess the nbr";
+console.log(`v short3 ${item2}`);
